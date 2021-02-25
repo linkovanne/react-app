dbdcc6e47387905b5df8245fb6236cbab8435da2
linkovanne <linkovanne@yandex.ru>
2021-02-25 18:37:55 +0300

    добавлена конфигурация webpack сервера

---
+++ conf/webpack.client.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+const NODE_ENV = process.env.NODE_ENV
+const IS_DEV = NODE_ENV === 'development'
+
+function setupDevtool() {
+    return IS_DEV ? 'eval' : false;
+}
+
+module.exports = {
+    mode: NODE_ENV ? NODE_ENV : 'development',
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json',]
+    },
+    entry: path.resolve(__dirname, '../src/client/index.jsx'),
+    output: {
+        path: path.resolve(__dirname, '../dist/client'),
+        filename: "client.js",
+    },
+    module: {
+        rules: [{
+            test: /\.[tj]sx?$/,
+            use: ['ts-loader']
+        }]
+    },
+    devtool: setupDevtool(),
+};
---
+++ src/server/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import reactDOM from 'react-dom/server';
+import {Header} from '../shared/Header'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(
+        reactDOM.renderToString(Header()),
+    );
+})
+
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+})
