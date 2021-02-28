bdb1f162d1ceadca7cd5560a336d2f45a1de2b13
linkovanne <linkovanne@yandex.ru>
2021-02-28 23:27:54 +0300

    подключение стилей

--- conf/webpack.server.config.js
+++ conf/webpack.server.config.js
@@ -30,8 +30,8 @@ module.exports = {
                             modules: {
                                 mode: 'local',
                                 localIdentName: '[name]__[local]--[hash:base64:5]',
+                                exportOnlyLocals: true,
                             },
-                            onlyLocals: true,
                         },
                     },
                     'less-loader',
